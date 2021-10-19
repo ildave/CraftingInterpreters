b8e83b53f0903f594423c0e8434fe8ebefe3b0d0
Davide Gualano <ildave@gmail.com>
2021-10-19 16:23:18 +0200

    Add interpeter execution to main

--- src/com/dave/craftinginterpreters/lox/Lox.java
+++ src/com/dave/craftinginterpreters/lox/Lox.java
@@ -10,7 +10,9 @@ import java.util.List;
 
 public class Lox {
 
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
     static boolean debug = true;
 
     public static void main(String[] args) throws IOException {
@@ -27,6 +29,8 @@ public class Lox {
     public static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+        if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     public static void runPrompt() throws IOException {
@@ -56,12 +60,12 @@ public class Lox {
 
         if (hadError) return;
         
-
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
 
         if (hadError) {
             System.exit(65);
         }
+
     }
 
     static void error(int line, String message) {
@@ -88,6 +92,11 @@ public class Lox {
         if (debug) {
             System.err.println(message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     
