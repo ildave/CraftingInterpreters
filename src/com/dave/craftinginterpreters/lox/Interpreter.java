5d8ff6d8ad2fc8d7013efd6032c04c1c1bf00ea7
Davide Gualano <ildave@gmail.com>
2021-10-20 11:46:01 +0200

    Add interpretation of statements

--- src/com/dave/craftinginterpreters/lox/Interpreter.java
+++ src/com/dave/craftinginterpreters/lox/Interpreter.java
@@ -1,6 +1,8 @@
 package com.dave.craftinginterpreters.lox;
 
-class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
+
+class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
     
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
@@ -75,6 +77,19 @@ class Interpreter implements Expr.Visitor<Object> {
         return null;
     }
 
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
+    }
+
     private void checkNumberOperands(Token operator, Object left, Object right) {
         if (left instanceof Double && right instanceof Double) return;
         throw new RuntimeError(operator, "Operands must be numbers.");
@@ -83,6 +98,10 @@ class Interpreter implements Expr.Visitor<Object> {
 
     public Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
     }
 
     private boolean isTruthy(Object object) {
@@ -110,10 +129,11 @@ class Interpreter implements Expr.Visitor<Object> {
         return object.toString();
     }
 
-    void interpret(Expr expression) {
+    void interpret(List<Stmt> statements) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error) {
             Lox.runtimeError(error);
         }
